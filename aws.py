ee2bee9d3d656647f8b513e262d3a8a009bf0d9c
Stephanie Silva <59899758+stesilva@users.noreply.github.com>
2025-03-29 12:35:31 +0100

    Updated Linkedin Logic

--- aws.py
+++ aws.py
@@ -1,6 +1,6 @@
 import boto3
 s3 = boto3.client('s3')
-bucket_name = 'linkedin-user-data-raw'
+bucket_name = 'linkedin-users-raw-data'
 local_file_path = 'data/profile_data.json'
 s3_file_name = 'profile_data.json'
 
