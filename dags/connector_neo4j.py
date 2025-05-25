124352216c80c6af6d965df653676ab237c3c139
stesilva <stephanievgomes@gmail.com>
2025-05-25 21:02:39 +0200

    explotation zone: User and Alumni (Neo4j)

--- dags/connector_neo4j.py
+++ dags/connector_neo4j.py
@@ -1,15 +1,14 @@
 from neo4j import GraphDatabase
 
-#Functions to connect to the Neo4j database and perform commands
 class ConnectorNeo4j:
-    def __init__(self, uri, user, password):
+    def __init__(self, uri, user, password, database="neo4j"):
         self._uri = uri
-        self._auth = (user,password)
+        self._auth = (user, password)
         self._driver = None
+        self._database = database  # banco padrão
 
     def connect(self):
         self._driver = GraphDatabase.driver(self._uri, auth=self._auth)
-
         try:
             self._driver.verify_connectivity()
             print("Connected to Neo4j successfully!")
@@ -22,13 +21,11 @@ class ConnectorNeo4j:
             print("Connection closed.")
 
     def create_session(self):
-        session = self._driver.session()
-        print("Neo4j session created.")
+        # Cria a sessão especificando o banco de dados
+        session = self._driver.session(database=self._database)
+        print(f"Neo4j session created for database '{self._database}'.")
         return session
 
-    def clear_session(self,session):
-       session.run(
-        "MATCH (n) DETACH DELETE n"
-        )
-       print("Cleared all nodes and relationships in the database.")
-
+    def clear_session(self, session):
+        session.run("MATCH (n) DETACH DELETE n")
+        print("Cleared all nodes and relationships in the database.")
